3099edca202c316d78bcfe8b290d72bc1e044969
SedlarDavid <sedlardavid.cz@gmail.com>
2022-06-24 22:55:52 +0200

    power management activity

--- app/src/main/java/cz/sedlardavid/testera/android/MainActivity.kt
+++ app/src/main/java/cz/sedlardavid/testera/android/MainActivity.kt
@@ -1,11 +1,22 @@
 package cz.sedlardavid.testera.android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnPowerManagement = findViewById<Button>(R.id.btnPowerManagement)
+        btnPowerManagement.setOnClickListener {
+            val intent = Intent(this, PowerManagementActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/cz/sedlardavid/testera/android/PowerManagementActivity.kt
@@ -0,0 +1,18 @@
+package cz.sedlardavid.testera.android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PowerManagementActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_power_management)
+    }
+}
+
+
+
+
+
+
