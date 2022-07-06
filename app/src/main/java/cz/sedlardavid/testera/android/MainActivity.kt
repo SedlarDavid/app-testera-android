93d4b2ca0ea80e1646a90dd73d7d4607012532ca
SedlarDavid <sedlardavid.cz@gmail.com>
2022-07-06 12:57:47 +0200

    compose base added, first compose component

--- app/src/main/java/cz/sedlardavid/testera/android/MainActivity.kt
+++ app/src/main/java/cz/sedlardavid/testera/android/MainActivity.kt
@@ -4,6 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,5 +30,37 @@ class MainActivity : AppCompatActivity() {
 
             startActivity(intent)
         }
+
+        val greeting = findViewById<ComposeView>(R.id.textView)
+        greeting.setContent {
+            MaterialTheme { // or AppCompatTheme
+                Greeting()
+            }
+        }
     }
+}
+
+@Composable
+private fun Greeting() {
+    Text(
+        text = stringResource(R.string.app_name),
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 50.dp, vertical = 50.dp)
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
+}
+
+@Composable
+@Preview
+private fun GreetingPreview() {
+    Text(
+        text = stringResource(R.string.app_name),
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 50.dp, vertical = 50.dp)
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
 }
